3f51a1bc51001ba8cc2ef85c70f2cba5deefed82
sininen-blue <alaanneil@gmail.com>
2023-10-26 19:57:10 +0800

    finished the PRG thing

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,19 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 
+final_chapter_content = []
+
 @app.get("/")
 def index_get():
     return render_template("index.html")
 
 
-@app.post("/")
-def index_post():
+@app.post("/process")
+def process():
     chapter_url = request.form["chapter_url"]
-    final_chapter_content = []
 
     page = requests.get(chapter_url)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -26,4 +27,9 @@ def index_post():
             # remove the text from the span and put it into a block
             final_chapter_content.append(block)
 
+    return redirect(url_for('page'))
+
+
+@app.get("/page")
+def page():
     return render_template("index.html", chapter_content=final_chapter_content)
