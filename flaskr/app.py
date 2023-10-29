f236a2655453f097e9bdb6ec1b5bad5573a5e810
sininen-blue <alaanneil@gmail.com>
2023-10-29 20:08:44 +0800

    create app for gunicorn

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,40 +1,59 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 
-final_chapter_content = []
+def create_app():
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
 
-@app.get("/")
-def index_get():
-    return render_template("index.html")
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route("/hello")
+    def hello():
+        return "Hello, World!"
 
 
-@app.post("/process")
-def process():
-    final_chapter_content.clear()
+    final_chapter_content = []
     
-    chapter_url = request.form["chapter_url"]
-
-    page = requests.get(chapter_url)
-    soup = BeautifulSoup(page.content, "html.parser")
-
-    chapter_content = soup.find(class_="chapter-content")
-    chapter_content = soup.find_all("p")
-
-    chapter_title = soup.find(class_="break-word").text
-
-    final_chapter_content.append(f"<h1>{chapter_title}</h1>")
     
-    for block in chapter_content:
-        if block.text.isspace() is False:
-            final_chapter_content.append(block)
-
-    return redirect(url_for('page'))
-
-
-@app.get("/page")
-def page():
-    return render_template("index.html", chapter_content=final_chapter_content)
+    @app.get("/")
+    def index_get():
+        return render_template("index.html")
+    
+    
+    @app.post("/process")
+    def process():
+        final_chapter_content.clear()
+    
+        chapter_url = request.form["chapter_url"]
+    
+        page = requests.get(chapter_url)
+        soup = BeautifulSoup(page.content, "html.parser")
+    
+        chapter_content = soup.find(class_="chapter-content")
+        chapter_content = soup.find_all("p")
+    
+        chapter_title = soup.find(class_="break-word").text
+    
+        final_chapter_content.append(f"<h1>{chapter_title}</h1>")
+    
+        for block in chapter_content:
+            if block.text.isspace() is False:
+                final_chapter_content.append(block)
+    
+        return redirect(url_for("page"))
+    
+    
+    @app.get("/page")
+    def page():
+        return render_template("index.html", chapter_content=final_chapter_content)
+    
+    return app
